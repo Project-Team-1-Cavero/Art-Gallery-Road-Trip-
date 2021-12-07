415d21aac499a749e230b9609f8138337a4fff88
Achraf Faress <a.faress.010@gmail.com>
2021-12-07 15:24:39 +0100

    added a list page
    Most of the function ar not inside yet

---
+++ artgallery/lib/screens/gallery_page/gallery_page_widgets/art_card.dart
@@ -0,0 +1,163 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class artCard extends StatefulWidget {
+
+  final QueryDocumentSnapshot<Object?> artpice;
+
+  artCard(this.artpice);
+
+  @override
+  _artCardState createState() => _artCardState();
+}
+
+class _artCardState extends State<artCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        height: MediaQuery.of(context).size.height / 10,
+        width: MediaQuery.of(context).size.width / 1.25,
+        child: Row(
+          children: [
+            
+            //Picture container
+            Container(
+              decoration: const BoxDecoration(color: Colors.black),
+              height: MediaQuery.of(context).size.height / 7.5,
+              width: MediaQuery.of(context).size.width / 3.125,
+            ),
+
+            // Info container
+            Container(
+              decoration: const BoxDecoration(color: Colors.white),
+              height: MediaQuery.of(context).size.height / 7.5,
+              width: MediaQuery.of(context).size.width / 1.938,
+              child: Row(
+                children: [
+
+                  //Left space
+                  Container(
+                    width: MediaQuery.of(context).size.width / 75,
+                  ),
+
+                  Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    //Upper information
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                           height: MediaQuery.of(context).size.height / 1000,
+                        ),
+                        Text(
+                          widget.artpice["Name"],
+                          style: TextStyle(
+                            color: HexColor("#4C6A7F"),
+                            fontSize: 30
+                          ),
+                        ),
+                        Text("Beschrijving over de kunstwerk", style: TextStyle(fontSize: 10),)
+                      ],
+                    ),
+
+                    Container(
+                      height: MediaQuery.of(context).size.height / 300,
+                    ),
+
+                    //Lower information
+                    Row(
+                      children: [
+
+                        //Artiest info
+                        Column(
+                          children: [
+                            const Text(
+                              "ARTIEST",
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            Text(widget.artpice["Artiest"], style: TextStyle(fontSize: 10,color: HexColor("#4C6A7F"),),)
+                          ],
+                        ),
+
+                        //Gold between
+                        Container(
+                          width: MediaQuery.of(context).size.width / 50,
+                          height: MediaQuery.of(context).size.height / 30,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 125,
+                          height: MediaQuery.of(context).size.height / 30,
+                          decoration: BoxDecoration(color: HexColor("#A1813D")),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 50,
+                          height: MediaQuery.of(context).size.height / 30,
+                        ),
+                        
+                        //jaar info
+                        Column(
+                          children: [
+                            const Text(
+                              "JAAR",
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            Text(widget.artpice["Year"], style: TextStyle(fontSize: 10,color: HexColor("#4C6A7F"),),)
+                          ],
+                        ),
+
+                        //Gold between
+                        Container(
+                          width: MediaQuery.of(context).size.width / 50,
+                          height: MediaQuery.of(context).size.height / 30,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 125,
+                          height: MediaQuery.of(context).size.height / 30,
+                          decoration: BoxDecoration(color: HexColor("#A1813D")),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 50,
+                          height: MediaQuery.of(context).size.height / 30,
+                        ),
+                        
+                        //Ruimte info
+                        Column(
+                          children: [
+                            const Text(
+                              "RUIMTE",
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            Text(widget.artpice["Room"], style: TextStyle(fontSize: 10,color: HexColor("#4C6A7F"),),)
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                ]
+              ),
+            ),
+
+            // More buttom container
+            Container(
+              decoration: BoxDecoration(color: HexColor("#D5CEBF")),
+              height: MediaQuery.of(context).size.height / 7.5,
+              width: MediaQuery.of(context).size.width / 7,
+            )
+
+           ],
+          ),
+       ),
+    );
+  }
+}
---
+++ artgallery/lib/screens/gallery_page/main_gallery_page.dart
@@ -0,0 +1,42 @@
+import 'package:artgallery/screens/gallery_page/gallery_page_subsetions/middel_gallery_page.dart';
+import 'package:artgallery/screens/gallery_page/gallery_page_subsetions/navigation_gallery_page.dart';
+import 'package:artgallery/screens/gallery_page/gallery_page_widgets/art_card.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class galleryPage extends StatefulWidget {
+  galleryPage({Key? key}) : super(key: key);
+
+  @override
+  _galleryPageState createState() => _galleryPageState();
+}
+
+class _galleryPageState extends State<galleryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: HexColor("#385051")), 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height /5,
+              decoration: BoxDecoration(color: Colors.red),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.67,
+              child: gallerList(),
+            ),
+            galleryNavigation()
+
+          ],
+        ),
+      ),
+    );
+  }
+}
