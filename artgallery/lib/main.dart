69966f6baf056b7c028b6233902f4c29d5010d35
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2022-01-05 17:08:43 +0100

    Revert "Merge branch 'main' of https://github.com/Project-Team-1-Cavero/Art-Gallery-Road-Trip-"
    This reverts commit 54981f97dc6d4556947c836b6649e7d5dcb8a41a, reversing
    changes made to 062d8bd10bf2323ff18956ce68983223daad6317.

--- artgallery/lib/main.dart
+++ artgallery/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:artgallery/screens/home/homepage.dart';
-import 'package:artgallery/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
   runApp(const MyApp());
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: splashScreen(),
+      home: homePage(),
     );
   }
 }
